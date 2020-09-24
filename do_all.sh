978478f2aaf78569ffc1912da2f532c8d48b53bf
Christian Mann <christian.mann91@gmail.com>
2020-09-24 18:49:18 -0400

    Added reset capability to do_all script since sometimes input does not quite work correctly

--- do_all.sh
+++ do_all.sh
@@ -4,7 +4,11 @@ while true; do
 	adb exec-out screencap -p > screen.png
 
 	python3 solve.py
+	if [ $? -gt 0 ]; then
+		# try resetting
+		adb shell input tap 550 1800
+	fi
 
 	adb shell input tap 700 1800
-	sleep 1
+	sleep 0.5
 done
