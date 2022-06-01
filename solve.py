7e68943587580993c5e83b56b60837d42f376aa0
Christian Mann <christian-mann@utulsa.edu>
2022-05-31 20:09:23 -0400

    Updates to solve.py to facilitate rectangular puzzles... need to modify pyflowsolver.py as well

--- solve.py
+++ solve.py
@@ -18,17 +18,21 @@ import sys
 
 # could do SAT solver... but let's try to make our own algorithm
 
+
 class GridReader:
     START_X = 0.0
-    START_Y = 391 / 1914
     END_X = 1.0
-    END_Y = 1 - 456 / 1920
 
     def __init__(self, fname):
         self.img = cv2.imread(fname)
         self.sheight, self.swidth = self.img.shape[:2]
         print(f'{self.sheight=}, {self.swidth=}')
-        self.grid_size = self.determine_grid_size()
+        self.gridline_color = self.determine_gridline_color()
+        print(f'{self.gridline_color=}')
+        self.gwidth, self.gheight = self.determine_grid_size()
+        print(f'{self.gheight=}, {self.gwidth=}')
+        self.starty, self.endy = self.determine_ybound()
+        print(f'{self.starty=}, {self.endy=}')
 
     def is_same_color(self, k, k2):
         if abs(k[0] - k2[0]) > 2: return False
@@ -36,13 +40,36 @@ class GridReader:
         if abs(k[2] - k2[2]) > 2: return False
         return True
 
+    def get_num_strings(self, pixels, target):
+        # aaBBccBBBBddBBaB -> 4, since 4 groups of B
+        in_gridline = False
+        num_gridlines = 0
+        for k in pixels:
+            if self.is_same_color(k, target):
+                if in_gridline:
+                    # already in gridline, we don't care
+                    pass
+                else:
+                    # new gridline
+                    num_gridlines += 1
+                    in_gridline = True
+            else:
+                if in_gridline:
+                    # gridline ended
+                    in_gridline = False
+                else:
+                    # already not in a gridline
+                    pass
+        return num_gridlines
+                
     def determine_gridline_color(self):
         colors = [
             self.img[
-                self.sheight // 2,
+                self.sheight // 2 + 3,
                 i
             ] for i in range(20)
         ]
+        print(f'{colors=}')
         # look for 2-3 in a row
         prev_c = None
         count = 0
@@ -58,44 +85,46 @@ class GridReader:
         raise Exception("Could not identify gridline color")
 
     def determine_grid_size(self):
-        gridline_color = self.determine_gridline_color()
-        print(f'{gridline_color=}')
 
         num_gridlines = 0
         in_gridline = False
         # try and count the vertical lines
-        py = round(self.sheight * self.START_Y + 10)
-        for px in range(self.swidth):
-            k = self.img[py,px]
-            if self.is_same_color(k, gridline_color):
-                if in_gridline:
-                    # already in gridline, we don't care
-                    pass
-                else:
-                    # new gridline
-                    num_gridlines += 1
-                    in_gridline = True
-            else:
-                if in_gridline:
-                    # gridline ended
-                    in_gridline = False
-                else:
-                    # already not in a gridline
-                    pass
-        return num_gridlines - 1
+        py = round(self.sheight * 0.5) + 3 # near the middle but not exactly
+        num_vlines = self.get_num_strings(
+            (self.img[py, x] for x in range(self.swidth)),
+            self.gridline_color
+        )
+        px = round(self.swidth * 0.5) + 3
+        num_hlines = self.get_num_strings(
+            (self.img[y, px] for y in range(self.sheight)),
+            self.gridline_color
+        )
+        print(f'{num_vlines=}, {num_hlines=}')
+        return (num_vlines - 1, num_hlines - 1)
+
+    def determine_ybound(self):
+        # just the first and last instance of gridline_color
+        first = None
+        last = None
+        for y in range(self.sheight):
+            if self.is_same_color(self.gridline_color, self.img[y, 10]):
+                if not first:
+                    first = y
+                last = y
+        return (first, last)
 
     def read_dots(self):
         # produces List[Pair[Coordinate]]
 
         # get the color at the center of each gridline
-        dx = self.swidth / self.grid_size
-        dy = self.sheight * (self.END_Y - self.START_Y) / self.grid_size
+        dx = self.swidth * (self.END_X - self.START_X) / self.gwidth
+        dy = (self.endy - self.starty) / self.gheight
 
         dots = defaultdict(list)
-        for y in range(self.grid_size):
-            for x in range(self.grid_size):
+        for y in range(self.gheight):
+            for x in range(self.gwidth):
                 px = x * dx + dx/2
-                py = y * dy + dy/2 + self.START_Y * self.sheight
+                py = y * dy + dy/2 + self.starty
 
                 px = round(px)
                 py = round(py)
@@ -110,8 +139,7 @@ class GridReader:
         # examine the dots array and make sure it makes sense
         for color, coords in dots.items():
             if len(coords) != 2:
-                print(f"Error: more than 2 dots of color {color}: {coords}")
-                return None
+                raise Exception(f"Error: more than 2 dots of color {color}: {coords}")
         return list(dots.values())
 
 class FakeArg:
@@ -121,10 +149,11 @@ class FakeArg:
         self.display_color = False
 
 class FlowSolver:
-    def __init__(self, size):
+    def __init__(self, gwidth, gheight):
         self.paths = []
         self.heads = []
-        self.size = size
+        self.gwidth = gwidth
+        self.gheight = gheight
         self.grid = defaultdict(lambda: 0)
         self.solved_names = []
         pass
@@ -136,11 +165,11 @@ class FlowSolver:
 
     def solve(self):
         # convert to pyflowsolver's format
-        square = [ ['.'] * self.size for _ in range(self.size) ]
+        square = [ ['.'] * self.gwidth for _ in range(self.gheight) ]
         for ((x,y), name) in self.heads:
-            square[x][y] = name[0].upper()
+            square[y][x] = name[0].upper()
         puzz_input = '\n'.join(''.join(L) for L in square)
-        #print(puzz_input)
+        print(puzz_input)
 
         options = FakeArg()
         puzzle, colors = pyflowsolver.parse_puzzle(options, puzz_input)
@@ -172,10 +201,8 @@ class GridInputter:
     # fractions of the screen that the grid takes up
     START_X = GridReader.START_X
     END_X = GridReader.END_X
-    START_Y = GridReader.START_Y
-    END_Y = GridReader.END_Y
 
-    def __init__(self, grid_size, host='192.168.1.179', port=5555):
+    def __init__(self, reader, host='192.168.1.179', port=5555):
         with open('adbkey') as f:
             priv = f.read()
         with open('adbkey.pub') as f:
@@ -183,9 +210,10 @@ class GridInputter:
         signer = PythonRSASigner(pub, priv)
         self.dev = AdbDeviceTcp(host, port, default_transport_timeout_s=9.)
         self.dev.connect(rsa_keys=[signer])
-        self.swidth, self.sheight = self.screen_dimensions()[::-1]
-        #print(f'{self.swidth=}, {self.sheight=}')
-        self.grid_size = grid_size
+        self.gr = reader
+        self.swidth, self.sheight = gr.swidth, gr.sheight
+        self.gwidth, self.gheight = gr.gwidth, gr.gheight
+        self.starty, self.endy = gr.starty, gr.endy
 
     def __del__(self):
         self.dev.close()
@@ -231,11 +259,11 @@ class GridInputter:
         # translate grid point to pixel offset
         x,y = point
 
-        dx = self.swidth / self.grid_size
-        dy = self.sheight * (self.END_Y - self.START_Y) / self.grid_size
+        dx = self.swidth / self.gwidth
+        dy = (self.endy - self.starty) / self.gheight
 
         px = x * dx + dx/2
-        py = y * dy + dy/2 + self.START_Y * self.sheight
+        py = y * dy + dy/2 + self.starty
 
         print(f"{x,y} becomes {px,py}")
         return round(px), round(py)
@@ -243,11 +271,10 @@ class GridInputter:
 
 if __name__ == '__main__':
     gr = GridReader('screen.png')
-    grid_size = gr.grid_size
 
     pairs = gr.read_dots()
 
-    fs = FlowSolver(grid_size)
+    fs = FlowSolver(gr.gwidth, gr.gheight)
     for pair, name in zip(pairs, string.ascii_lowercase):
         fs.add_pair(pair, name)
     print()
@@ -260,7 +287,7 @@ if __name__ == '__main__':
         raise
 
     if fs.is_solved():
-        gi = GridInputter(grid_size)
+        gi = GridInputter(gr.gwidth, gr.gheight)
         for path in fs.paths:
             path = gi.simplify_path(path)
             print(fs.grid[path[0]])
